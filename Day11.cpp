746425a1bc2cd94d7dc3f8d66c8253e5dd0847ef
Saloni Nigam <saloninigam2311@gmail.com>
2026-06-24 11:11:53 +0530

    Day 11: While, Do While loop, switch, break, continue

--- Day11.cpp
+++ Day11.cpp
@@ -152,37 +152,37 @@
 
 // // switch 
 
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int i=1;
-//     switch(i)
-//     {
-//         case 1:
-//         cout<<" monday";
-//         break;
-//         case 2:
-//         cout<<" tuesday";
-//         break;
-//         case 3:
-//         cout<<"wed";
-//         break;
-//         case 4:
-//         cout<<" thru";
-//         break;
-//         case 5:
-//         cout<<" fri";
-//         break;
-//         case 6:
-//         cout<<" sat";
-//         break;
-//         case 7:
-//         cout<<"sun";
-//         break;
-//         default:
-//         cout<<"error";
-//         break;
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    switch(i)
+    {
+        case 1:
+        cout<<" monday";
+        break;
+        case 2:
+        cout<<" tuesday";
+        break;
+        case 3:
+        cout<<"wed";
+        break;
+        case 4:
+        cout<<" thru";
+        break;
+        case 5:
+        cout<<" fri";
+        break;
+        case 6:
+        cout<<" sat";
+        break;
+        case 7:
+        cout<<"sun";
+        break;
+        default:
+        cout<<"error";
+        break;
+    }
+}
 
