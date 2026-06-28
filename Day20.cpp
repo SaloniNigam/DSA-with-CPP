6174ab4381a64e214760e6158001fa7de24bf62c
Saloni Nigam <saloninigam2311@gmail.com>
2026-06-28 10:47:00 +0530

    Day 20: Bubble Sort

--- Day20.cpp
+++ Day20.cpp
@@ -1,40 +1,40 @@
-// // Bubble sort 
+// Bubble sort 
 
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//    // arr input from user
-//     int arr[1000];
-//     int n;
-//     cout<<"enter the size of array ";
-//     cin>>n;
-//     cout<<"Enter the element of array ";
-//     for(int i=0;i<n;i++)
-//     cin>>arr[i];
+#include<iostream>
+using namespace std;
+int main()
+{
+   // arr input from user
+    int arr[1000];
+    int n;
+    cout<<"enter the size of array ";
+    cin>>n;
+    cout<<"Enter the element of array ";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
 
    
-//     for(int i=n-2;i>=0;i--)
-//     {
-//         bool swapped=0;
+    for(int i=n-2;i>=0;i--)
+    {
+        bool swapped=0;
     
-//     for(int j=0;j<=i;j++)
-//     {
-//         if(arr[j]>arr[j+1]) // for ascending order
-//         //if(arr[j]<arr[j+1]) // for desending order
-//         swap(arr[j],arr[j+1]);
-//         swapped=1;
+    for(int j=0;j<=i;j++)
+    {
+        if(arr[j]>arr[j+1]) // for ascending order
+        //if(arr[j]<arr[j+1]) // for desending order
+        swap(arr[j],arr[j+1]);
+        swapped=1;
         
-//     }
-//     if (swapped==0)
-//     break;
-// }
+    }
+    if (swapped==0)
+    break;
+}
 
-// // print the array
-// for(int i=0;i<n;i++){
-// cout<<arr[i]<<" ";
-// }
-// }
+// print the array
+for(int i=0;i<n;i++){
+cout<<arr[i]<<" ";
+}
+}
 
 
 // Bubble sort from backward
