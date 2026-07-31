54b0987b6b183372cc8cdbbeb03c1f206828cc98
Saloni Nigam <saloninigam2311@gmail.com>
2026-07-31 11:00:12 +0530

    Day 41: Smallest Distinct Window

---
+++ Day41_SmallestDistinctWindow.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    cout<<"Enter the string: ";
+    cin>>str;
+
+    vector<int>count(256,0);
+        int first=0,second=0,len=str.size(),diff=0;
+        
+        while(first<str.size())
+        {
+            if(count[str[first]]==0)
+           { diff++;
+           }
+            count[str[first]]++;
+            first++;
+           
+        }
+        
+        for(int i=0;i<256;i++)
+        {
+            count[i]=0;
+           
+        }
+         first=0;
+        while(second<str.size())
+        {
+        while(diff && second<str.size())
+        {
+            if(count[str[second]]==0)
+            diff--;
+            count[str[second]]++;
+            second++;
+        }
+        len=min(len,second-first);
+        while(diff!=1)
+        {
+            len=min(len,second-first);
+            count[str[first]]--;
+            if(count[str[first]]==0)
+            
+            diff++;
+            first++;
+        }
+        }
+        
+        cout<<"Length of smallest distinct string is: "<< len;
+}
